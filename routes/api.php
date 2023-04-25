8a2c27868a854f3f80f4306799f88f03ddb3ecfe
Maxwell <mboakye@excenit.com>
2023-04-25 22:59:32 +0000

    added api route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['middleware'=>'api',
+              'prefix'=>'v1'
+],function($router){
+
+
+    Route:: post("admin-signup","App\Http\Controllers\AdminController@adminSignUp");
+
 });
