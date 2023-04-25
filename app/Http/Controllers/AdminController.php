a578131d86e3e2ad151ca31d6c696e7048d2f213
Maxwell <mboakye@excenit.com>
2023-04-25 23:13:52 +0000

    added login api route

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -18,7 +18,7 @@ class AdminController extends Controller
      }
 
     public function __construct(){
-        $this->middleware('auth:api', ['except'=>['adminSignUp',]]);
+        $this->middleware('auth:api', ['except'=>['adminSignUp','adminLogin']]);
     }
 
 
