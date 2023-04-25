435e7f77786f8332b8d4768ca52f0cf2da6fb26c
Maxwell <mboakye@excenit.com>
2023-04-25 23:08:44 +0000

    added login api route

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -61,6 +61,38 @@ class AdminController extends Controller
         return $this-> createNewToken1($token);
 
 }
+
+
+
+
+public function adminLogin(Request $request){
+    $validator = Validator::make($request->all(), [
+        'email'=> ['required','string'],
+        'password' => ['required','string',
+         Password::min(8)->mixedCase()->numbers()->symbols()->uncompromised()],
+    ]);
+
+    if($validator->stopOnFirstFailure()-> fails()){
+        return $this->sendResponse([
+            'success' => false,
+            'data'=> $validator->errors(),
+            'message' => 'Validation Error'
+        ], 400);
+    }
+
+    if(!$token = auth()->guard('admin-api')->attempt($validator->validated())){
+        return $this->sendResponse([
+            'success' => false,
+            'data'=> $validator->errors(),
+            'message' => 'Invalid login credentials'
+        ], 400);
+
+    }
+
+     return $this-> createNewToken($token);
+}
+
+
 public function createNewToken1($token){
     return response()->json([
         // 'success'=>'true',
@@ -72,4 +104,13 @@ public function createNewToken1($token){
     ]);
 }
 
+public function createNewToken($token){
+    return response()->json([
+        'access_token' => $token,
+        'token_type' => 'bearer',
+        'expires_in' => config('jwt.ttl') * 60,
+        'user'=>auth()->guard('admin-api')->user(),
+        'message'=>'Logged in successfully.'
+    ]);
+}
 }
